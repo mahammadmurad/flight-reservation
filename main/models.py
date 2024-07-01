bac788db882fb45506ac5f2f08bc4b95019819c8
mahammadmurad <mahammad.muradd@gmail.com>
2024-07-01 16:58:38 +0400

    student serializer created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    score = models.DecimalField(max_digits=10, decimal_places=3)
+    
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from main.models import Student
+from main.serailizers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def student_list(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many= True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def student_detail(request,pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serailizer = StudentSerializer(student)
+        return Response(serailizer.data)
+    elif request.method == 'PUT':
+        serailizer = StudentSerializer(student, data=request.data)
+        if serailizer.is_valid():
+            serailizer.save()
+            return Response(serailizer.data)
+        return Response(serailizer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
